104ab42d26555a81cd2a0aab1d0355549bf7acdc
fppo.costalli <filppocostalligmail.com>
2022-05-12 15:48:32 +0200

    Comment block removed

--- tradebot/src/main/java/it/costalli/tradebot/model/Account.java
+++ tradebot/src/main/java/it/costalli/tradebot/model/Account.java
@@ -18,19 +18,5 @@ public class Account<T> {
 	private final T accountId;
 	private final double amountAvailableRatio; /*The amount available to trade as a fraction of total amount*/
 	private final double marginRate;           /*The leverage offered on this account. for e.g. 0.05, 0.1 etc*/
-	
-	
-	
-	/*
-	 * account = new Account<String>(
-										accountMarginUsed, 
-					accountMarginAvailable, 
-					accountOpenTrades, 
-					accountBaseCurrency, 
-					accountId,
-					accountAmountAvailableRatio,
-					accountLeverage);
-	 */
-	
 
 }
